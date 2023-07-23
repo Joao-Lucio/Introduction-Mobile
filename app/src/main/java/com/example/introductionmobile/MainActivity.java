a26a222cf0006e09f18646f59f9b1120b749caf7
Joao-Lucio <joao.silvarrr@gmail.com>
2023-07-23 00:01:11 -0400

    Create Layout

--- app/src/main/java/com/example/introductionmobile/MainActivity.java
+++ app/src/main/java/com/example/introductionmobile/MainActivity.java
@@ -11,67 +11,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 1;
-    TextView quantityText, totalMessage;
-    Button order, incrementButton, decrementButton;
-    String message;
-
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        incrementQuantity();
-        decrementQuantity();
-        orderButton();
-
-        quantityText = findViewById(R.id.quantity);
-        message = "Your total is $ ";
-        totalMessage = findViewById(R.id.totalMessage);
-        totalMessage.setVisibility(View.GONE);
-    }
-
-    private void incrementQuantity(){
-        incrementButton = findViewById(R.id.increment);
-
-        incrementButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(quantity < 10){
-                quantity++;
-                }
-                quantityText.setText(String.valueOf(quantity));
-            }
-        });
-    }
-
-    private void decrementQuantity(){
-        decrementButton = findViewById(R.id.decrement);
-
-        decrementButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(quantity > 1){
-                quantity--;
-                }
-
-                quantityText.setText(String.valueOf(quantity));
-            }
-        });
-    }
-    private void orderButton(){
-        order = findViewById(R.id.order);
-        EditText edName = findViewById(R.id.edName);
-        order.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int totalNumber = quantity * 15;
-
-                totalMessage.setText(message + totalNumber + "!" + "\n" + " Thanks for buying with us, " + edName.getText().toString());
-                totalMessage.setVisibility(View.VISIBLE);
-            }
-        });
     }
 }
