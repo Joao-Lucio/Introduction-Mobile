c1658238535666305264afc23ef5715d747e40bf
Joao-Lucio <joao.silvarrr@gmail.com>
2023-07-23 01:04:14 -0400

    Create rocketModel

--- app/src/main/java/com/example/introductionmobile/MainActivity.java
+++ app/src/main/java/com/example/introductionmobile/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,13 +13,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    RecyclerView recyclerView;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.view);
 
     }
 
